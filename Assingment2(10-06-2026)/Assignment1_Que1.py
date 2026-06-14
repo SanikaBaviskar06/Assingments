f94107f6235b357a4d682d75aec4e29b22d02c02
SanikaBaviskar06 <sanikabaviskar6@gmail.com>
2026-06-14 13:08:30 +0530

    Assingment-2

---
+++ Assingment2(10-06-2026)/Assignment1_Que1.py
@@ -0,0 +1,4 @@
+No = 10
+print("Value : " , No)
+print("Type :",type(No))
+print("Memory Address :",id(No))
---
+++ Assingment2(10-06-2026)/Assignment4_Que4.py
@@ -0,0 +1,4 @@
+import sys
+print(sys.getsizeof(10))
+print(sys.getsizeof("Marvellous"))
+print(sys.getsizeof([10,20,30]))
